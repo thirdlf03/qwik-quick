6387cfdb218c4931a451309d71a95c51c8b1f372
thirdlf03 <thirdlf03@gmail.com>
2024-12-23 11:56:24 +0900

    comp tutorial

--- src/routes/hello/index.tsx
+++ src/routes/hello/index.tsx
@@ -1,13 +1,30 @@
-import { component$ } from '@builder.io/qwik'
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { component$, useSignal, useTask$, useStylesScoped$ } from '@builder.io/qwik'
+import { routeLoader$, Form, routeAction$, server$ } from '@builder.io/qwik-city';
+import styles from "./index.css?inline";
 
 export const useZennArticle = routeLoader$(async () => {
     const response = await fetch("https://zenn.dev/api/articles?username=thirdlf&order=latest");
     return response.json();
 })
 
+export const useVoteAction = routeAction$((props) => {
+    console.log('VOTE', props);
+});
+
 export default component$(() => {
+    useStylesScoped$(styles);
+    const isLike = useSignal(false);
     const zennArticles = useZennArticle();
+    const voteAction = useVoteAction();
+
+    useTask$(({ track }) => {
+        track(() => isLike.value);
+        console.log('(isomorphic)', isLike.value);
+        server$(() => {
+            console.log('(server)', isLike.value);
+        })();
+    });
+
     return (
         <div>
             <h1>Hello, World!</h1>
@@ -15,7 +32,16 @@ export default component$(() => {
                 {zennArticles.value.articles.map((article: any) => (
                     <div key={article.id} align="center">
                         <p>記事名: {article.title}</p>
-                        <p>いいね数: {article.liked_count}</p>
+                        <button onClick$={() => {
+                            article.liked_count > 0 ? isLike.value = !isLike.value : isLike.value = false;
+                        }}>いいねチェック
+                        </button>
+                        {isLike.value ? <p>いいね数: {article.liked_count}</p> : <p>いいね数チェックしてね</p>}
+                        <Form action={voteAction}>
+                            <input type="hidden" name="voteID" value={article.id} />
+                            <button name="vote" value="up">👍</button>
+                            <button name="vote" value="down">👎</button>
+                        </Form>
                     </div>
                 ))}
             </div>
